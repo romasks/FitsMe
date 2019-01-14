7cee3abcc45329db2315f8d8208f49725489b7a0
Алексей Ельфимов <ya.chaosart@gmail.com>
2019-01-14 03:15:52 +0300

    MainActivity переделан в контейнер фрагментов

---
+++ app/src/main/java/ru/fitsme/android/Presentation/presenter/MainPresenter.java
@@ -0,0 +1,6 @@
+package ru.fitsme.android.Presentation.presenter;
+
+import android.arch.lifecycle.ViewModel;
+
+public class MainPresenter extends ViewModel {
+}
--- app/src/main/java/ru/fitsme/android/Presentation/view/MainActivity.java
+++ app/src/main/java/ru/fitsme/android/Presentation/view/MainActivity.java
@@ -1,5 +1,6 @@
 package ru.fitsme.android.Presentation.view;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -7,6 +8,7 @@ import javax.inject.Inject;
 
 import ru.fitsme.android.App.App;
 import ru.fitsme.android.Domain.Interactors.Auth.SignInUpInteractor;
+import ru.fitsme.android.Presentation.presenter.MainPresenter;
 import ru.fitsme.android.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,5 +24,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MainPresenter mainPresenter = ViewModelProviders.of(this)
+                .get(MainPresenter.class);
     }
 }
