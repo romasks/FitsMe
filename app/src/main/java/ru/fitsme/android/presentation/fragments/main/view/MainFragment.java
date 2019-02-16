30f2975b2e0f23e5f46087647538bb10e623d198
Igor <1itcode1@gmail.com>
2019-02-16 10:31:53 +0000

    1) create empty fragments

--- app/src/main/java/ru/fitsme/android/presentation/fragments/main/view/MainFragment.java
+++ app/src/main/java/ru/fitsme/android/presentation/fragments/main/view/MainFragment.java
@@ -11,8 +11,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import ru.fitsme.android.R;
+import ru.fitsme.android.presentation.fragments.basket.BasketFragment;
 import ru.fitsme.android.presentation.fragments.main.adapter.MainViewPageAdapter;
+import ru.fitsme.android.presentation.fragments.profile.view.ProfileFragment;
+import ru.fitsme.android.presentation.fragments.rateitems.view.RateItemsFragment;
 import timber.log.Timber;
 
 
@@ -42,7 +48,16 @@ public class MainFragment extends Fragment {
 
     private void initViewPager(View view) {
         viewPager = view.findViewById(R.id.vp_main_fr_container);
-        viewPager.setAdapter(new MainViewPageAdapter(getChildFragmentManager()));
+        viewPager.setAdapter(new MainViewPageAdapter(getChildFragmentManager(), createFragments()));
+    }
+
+
+    private List<Fragment> createFragments() {
+        List<Fragment> fragments = new ArrayList<>();
+        fragments.add(ProfileFragment.newInstance());
+        fragments.add(RateItemsFragment.newInstance());
+        fragments.add(BasketFragment.newInstance());
+        return fragments;
     }
 
     private void initBottomNavigation(View view) {
