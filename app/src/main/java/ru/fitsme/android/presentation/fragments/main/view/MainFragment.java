886d02c5f810fda8e58eaf58dd0c25bfa33e36e0
Igor <1itcode1@gmail.com>
2019-02-16 10:40:30 +0000

    1) fix main fragment bug

--- app/src/main/java/ru/fitsme/android/presentation/fragments/main/view/MainFragment.java
+++ app/src/main/java/ru/fitsme/android/presentation/fragments/main/view/MainFragment.java
@@ -36,7 +36,7 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_rate_item, container, false);
+        return inflater.inflate(R.layout.fragment_main, container, false);
     }
 
     @Override
