bebf6d926b603fd9eede5f451759d0fb8e8a8e88
Roman Skaskevich <roman.skaskevich@gmail.com>
2019-03-03 22:09:51 +0300

    Поправлено именование поля сообщения об ошибке

--- app/src/main/java/ru/fitsme/android/data/frameworks/retrofit/entities/Error.java
+++ app/src/main/java/ru/fitsme/android/data/frameworks/retrofit/entities/Error.java
@@ -2,13 +2,13 @@ package ru.fitsme.android.data.frameworks.retrofit.entities;
 
 public class Error {
     private int code;
-    private String message;
+    private String mesage;
 
     public int getCode() {
         return code;
     }
 
     public String getMessage() {
-        return message;
+        return mesage;
     }
 }
