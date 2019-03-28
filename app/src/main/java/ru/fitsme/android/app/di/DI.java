7eb6e56fe223084b23dd15b3ed1adfb68eaad82d
fOrgeth <ivdovin@mail.ru>
2019-03-28 10:01:57 +0300

    OkHttp interceptor added

--- app/src/main/java/ru/fitsme/android/app/di/DI.java
+++ app/src/main/java/ru/fitsme/android/app/di/DI.java
@@ -16,6 +16,7 @@ import io.reactivex.Scheduler;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -116,7 +117,11 @@ public class DI {
 
     @NonNull
     private OkHttpClient okHttpClient() {
-        return new OkHttpClient.Builder().build();
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        return new OkHttpClient.Builder()
+                .addInterceptor(interceptor)
+                .build();
     }
 
     public <T> void inject(T object) {
