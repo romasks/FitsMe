cc10058bfeaceb5fd2f90b126d4e6d103c3910bd
Roman Skaskevich <roman.skaskevich@gmail.com>
2019-04-08 18:06:42 +0300

    Добавил вью модель + небольшой рефакторинг

--- app/src/main/java/ru/fitsme/android/presentation/fragments/checkout/CheckoutFragment.java
+++ app/src/main/java/ru/fitsme/android/presentation/fragments/checkout/CheckoutFragment.java
@@ -1,7 +1,10 @@
 package ru.fitsme.android.presentation.fragments.checkout;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,13 +12,23 @@ import android.view.ViewGroup;
 
 import org.jetbrains.annotations.NotNull;
 
+import javax.inject.Inject;
+
 import ru.fitsme.android.R;
 import ru.fitsme.android.app.App;
 import ru.fitsme.android.databinding.FragmentCheckoutBinding;
+import ru.fitsme.android.domain.entities.order.Order;
+import ru.fitsme.android.domain.interactors.orders.IOrdersInteractor;
+
+import static ru.fitsme.android.utils.Constants.GONE;
+import static ru.fitsme.android.utils.Constants.VISIBLE;
 
 public class CheckoutFragment extends Fragment {
+    @Inject
+    IOrdersInteractor ordersInteractor;
 
     private FragmentCheckoutBinding binding;
+    private CheckoutViewModel viewModel;
 
     public CheckoutFragment() {
         App.getInstance().getDi().inject(this);
@@ -31,4 +44,21 @@ public class CheckoutFragment extends Fragment {
         return binding.getRoot();
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        viewModel = ViewModelProviders.of(this,
+                new CheckoutViewModel.Factory(ordersInteractor)).get(CheckoutViewModel.class);
+        if (savedInstanceState == null) {
+            viewModel.init();
+        }
+
+        viewModel.loading.set(VISIBLE);
+        viewModel.getOrderLiveData().observe(this, this::onLoadOrder);
+    }
+
+    private void onLoadOrder(Order order) {
+        viewModel.loading.set(GONE);
+    }
 }
---
+++ app/src/main/java/ru/fitsme/android/presentation/fragments/checkout/CheckoutViewModel.java
@@ -0,0 +1,70 @@
+package ru.fitsme.android.presentation.fragments.checkout;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
+import android.databinding.ObservableBoolean;
+import android.support.annotation.NonNull;
+
+import org.jetbrains.annotations.NotNull;
+
+import io.reactivex.disposables.Disposable;
+import ru.fitsme.android.domain.entities.order.Order;
+import ru.fitsme.android.domain.interactors.orders.IOrdersInteractor;
+import ru.fitsme.android.utils.OrderStatus;
+
+import static ru.fitsme.android.utils.Constants.GONE;
+
+public class CheckoutViewModel extends ViewModel {
+
+    private final String TAG = getClass().getName();
+    private final IOrdersInteractor ordersInteractor;
+
+    private MutableLiveData<Order> orderLiveData;
+    private Disposable disposable;
+
+    public ObservableBoolean loading;
+
+    private CheckoutViewModel(@NotNull IOrdersInteractor ordersInteractor) {
+        this.ordersInteractor = ordersInteractor;
+    }
+
+    void init() {
+        loading = new ObservableBoolean(GONE);
+        loadOrder();
+    }
+
+    private void loadOrder() {
+        disposable = ordersInteractor.getSingleOrder(1)
+                .subscribe((order) -> {
+                    orderLiveData.setValue(order);
+                }, throwable -> {
+                });
+    }
+
+    LiveData<Order> getOrderLiveData() {
+        return orderLiveData;
+    }
+
+    public void makeOrder() {
+        disposable = ordersInteractor.makeOrder("", "", OrderStatus.FM)
+                .subscribe(() -> {
+                }, throwable -> {
+                });
+    }
+
+    static public class Factory implements ViewModelProvider.Factory {
+        private final IOrdersInteractor ordersInteractor;
+
+        public Factory(@NotNull IOrdersInteractor ordersInteractor) {
+            this.ordersInteractor = ordersInteractor;
+        }
+
+        @NonNull
+        @Override
+        public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+            return (T) new CheckoutViewModel(ordersInteractor);
+        }
+    }
+}
