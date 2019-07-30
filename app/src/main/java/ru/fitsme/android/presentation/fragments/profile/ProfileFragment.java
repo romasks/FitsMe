663510a840b3b5afabda342f85316727d603faf3
taikonavt <taikonavt@gmail.com>
2019-07-30 09:49:25 +0300

    auth by phone xml. in process

--- app/src/main/java/ru/fitsme/android/presentation/fragments/profile/ProfileFragment.java
+++ app/src/main/java/ru/fitsme/android/presentation/fragments/profile/ProfileFragment.java
@@ -22,6 +22,6 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return inflater.inflate(R.layout.fragment_phone_auth, container, false);
     }
 }
