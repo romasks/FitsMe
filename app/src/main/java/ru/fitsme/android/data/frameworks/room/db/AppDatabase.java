b52d6272f3c7353f18f6c2b6463af3fa17de74d8
taikonavt <taikonavt@gmail.com>
2019-12-15 17:03:06 +0300

    fix database

--- app/src/main/java/ru/fitsme/android/data/frameworks/room/db/AppDatabase.java
+++ app/src/main/java/ru/fitsme/android/data/frameworks/room/db/AppDatabase.java
@@ -5,8 +5,11 @@ import android.content.Context;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import ru.fitsme.android.data.frameworks.room.RoomBrands;
+import ru.fitsme.android.data.frameworks.room.RoomColors;
+import ru.fitsme.android.data.frameworks.room.RoomProductName;
 
-@Database(entities = {}, version = 1)
+@Database(entities = {RoomBrands.class, RoomColors.class, RoomProductName.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
     private static final String DB_NAME = "database.db";
     private static volatile AppDatabase instance;
