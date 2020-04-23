472a3d6ae694df7b2d44d2b364cb047cd4703016
Raman Skaskevich <ras@novelpay.pl>
2020-04-24 01:44:26 +0200

    Fix field names

--- app/src/main/java/ru/fitsme/android/data/frameworks/retrofit/entities/ReturnsItemRequest.java
+++ app/src/main/java/ru/fitsme/android/data/frameworks/retrofit/entities/ReturnsItemRequest.java
@@ -1,7 +1,12 @@
 package ru.fitsme.android.data.frameworks.retrofit.entities;
 
+import com.google.gson.annotations.SerializedName;
+
 public class ReturnsItemRequest {
+
+    @SerializedName("orderitems_id")
     private int orderItemId;
+    @SerializedName("quantity")
     private int quantity;
 
     public ReturnsItemRequest(int orderItemId, int quantity) {
