48b01da2f3d3ab437422dd3406d44dc3700595b4
Maksim <taikonavt@gmail.com>
2020-05-07 22:40:09 +0300

    add sizes to detail itemInfo fragment

--- app/src/main/java/ru/fitsme/android/presentation/fragments/iteminfo/ItemInfoViewModel.java
+++ app/src/main/java/ru/fitsme/android/presentation/fragments/iteminfo/ItemInfoViewModel.java
@@ -1,10 +1,31 @@
 package ru.fitsme.android.presentation.fragments.iteminfo;
 
+import androidx.lifecycle.LiveData;
+
+import javax.inject.Inject;
+
+import ru.fitsme.android.domain.interactors.profile.IProfileInteractor;
 import ru.fitsme.android.presentation.fragments.base.BaseViewModel;
 
 public class ItemInfoViewModel extends BaseViewModel {
 
+    @Inject
+    IProfileInteractor profileInteractor;
+
     public ItemInfoViewModel() {
         inject(this);
+    }
+
+
+    public void init() {
+        profileInteractor.updateInfo();
+    }
+
+    public LiveData<String> getCurrentTopSize(){
+        return profileInteractor.getCurrentTopSize();
+    }
+
+    public LiveData<String> getCurrentBottomSize(){
+        return profileInteractor.getCurrentBottomSize();
     }
 }
